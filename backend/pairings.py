0a0e25392f0a449782338c3b8e00ccbe577f27fe
MozartofCode <bertan.berker@gmail.com>
2025-01-06 10:52:43 -0500

    Created the template functions for pairing, will use agents for the functions...

---
+++ backend/pairings.py
@@ -0,0 +1,28 @@
+# @Author: Bertan Berker
+# This file contains the functions that could be used for giving pairing recommendations
+# to the users
+
+
+# Class to represent a pairing of clothing items
+class Pairing:
+    def __init__(self, shoe, top, bottom, accessory):
+        self.shoe = shoe
+        self.top = top
+        self.bottom = bottom
+        self.accessory = accessory
+
+
+# Get the possible pairings for the current clothing items in the user's wardrobe
+# :param: None
+# :return: List of possible pairings for the current clothing items in the user's wardrobe
+def get_current_pairings():
+    return
+
+
+# This function will take in a list of new clothing items that the user is considering adding
+# to their wardrobe and will return a list of possible things that could be paired with the new
+# clothing items
+# :param new_clothing: List of the new clothing items that the user is considering adding
+# :return: List of possible things that could be paired with the new clothing items
+def get_new_pairings(new_clothing):
+    return
--- backend/wardrobe.py
+++ backend/wardrobe.py
@@ -2,3 +2,5 @@
 # @Language: Python3
 # Creates a wardrobe object that contains a list of clothes objects
 # by looking into a video of a wardrobe and detecting clothes using AI agents
+
+
